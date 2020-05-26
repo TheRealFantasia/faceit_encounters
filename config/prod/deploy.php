d67f6acb40bc246e570f7d8f370f170acfb7eb9e
Gordon Kirsch <therealfantasia@gmx.de>
2020-05-26 22:06:56 +0200

    deploy

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -1,32 +1,47 @@
 <?php
 
+use EasyCorp\Bundle\EasyDeployBundle\Configuration\Option;
 use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+use EasyCorp\Bundle\EasyDeployBundle\Requirement\AllowsLoginViaSsh;
+use EasyCorp\Bundle\EasyDeployBundle\Requirement\CommandExists;
+use EasyCorp\Bundle\EasyDeployBundle\Server\Server;
 
 return new class extends DefaultDeployer
 {
+    public function getRequirements(): array
+    {
+        $localhost = $this->getContext()->getLocalHost();
+        $allServers = $this->getServers()->findAll();
+
+        $requirements[] = new CommandExists([$localhost], 'git');
+        $requirements[] = new CommandExists([$localhost], 'ssh');
+
+        $requirements[] = new AllowsLoginViaSsh($allServers);
+
+        return $requirements;
+    }
+
     public function configure()
     {
         return $this->getConfigBuilder()
             ->server('root@212.227.213.145')
-            // the absolute path of the remote server directory where the project is deployed
             ->deployDir('/var/www/vhosts/faceit_encounters')
-            // the URL of the Git repository where the project code is hosted
-            ->repositoryUrl('https://github.com/TheRealFantasia/faceit_encounters')
-            // the repository branch to deploy
+            ->repositoryUrl('git@github.com:TheRealFantasia/faceit_encounters.git')
             ->repositoryBranch('master')
+            ->useSshAgentForwarding(true)
         ;
     }
 
-    // run some local or remote commands before the deployment is started
-    public function beforeStartingDeploy()
+    public function beforePreparing()
     {
-        // $this->runLocal('./vendor/bin/simple-phpunit');
+        $this->log('<h3>Copying over the .env files</>');
+        $this->runRemote('cp {{ deploy_dir }}/env/.env {{ project_dir }}/.env');
     }
 
-    // run some local or remote commands after the deployment is finished
     public function beforeFinishingDeploy()
     {
-        $this->runRemote('yarn build');
-        // $this->runLocal('say "The deployment has finished."');
+        $this->runRemote('{{ console_bin }} doctrine:migrations:diff')[0]->getOutput();
+        $this->runRemote('{{ console_bin }} doctrine:migrations:migrate');
+        $this->runRemote('cd {{ project_dir }} && yarn install');
     }
 };
